f1df5e230651ae2eb976e670b928bb314fa8a9d0
Vuong Tran M <vuong.tran@monstar-lab.com>
2021-04-06 14:44:34 +0700

    create list, form submit prevent reload page, remove item in list

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.scss';
 import ColorBox from './components/ColorBox';
 import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
 
 function App() {
   const [todoList, setTodoList] = useState([
@@ -22,10 +23,23 @@ function App() {
     newTodoList.splice(index, 1);
     setTodoList(newTodoList);
   }
+
+  function handleTodoFormSubmit(formValues) {
+    console.log('form submit', formValues);
+    // add new todo to current todo list
+    const newTodoList = [...todoList];
+    const newTodo = {
+      id: todoList.length + 1,
+      ...formValues
+    }
+    newTodoList.push(newTodo);
+    setTodoList(newTodoList);
+  }
   return (
     <div className="app">
       <h1>React Hooks Todo List</h1>
       <TodoList todos={todoList} onTodoClick={handleTodoClick} />
+      <TodoForm onSubmit={handleTodoFormSubmit} />
     </div>
   );
 }
---
+++ src/components/TodoForm/index.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+TodoForm.propTypes = {
+  onSubmit: PropTypes.func,
+};
+
+TodoForm.defaultProps = {
+  onSubmit: null,
+};
+
+function TodoForm(props) {
+  const { onSubmit } = props;
+  const [value, setValue] = useState('');
+
+  function handleValueChange(e) {
+    setValue(e.target.value);
+  };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!onSubmit) return;
+
+    const formValues = {
+      title: value,
+    };
+
+    onSubmit(formValues);
+
+    setValue('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name=""
+        value={value}
+        onChange={handleValueChange}
+      />
+
+    </form>
+  );
+}
+
+export default TodoForm;
